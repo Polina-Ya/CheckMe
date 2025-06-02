79c634f30000a61679a187e519d851059be8a778
Polina-Ya <era35900@yandex.ru>
2025-06-02 03:44:56 +0300

    Delete server directory

--- server/index.js
+++
@@ -1,4 +0,0 @@
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => res.send('CheckMe Backend работает'));
-app.listen(4000, () => console.log('Сервер запущен на порту 4000'));
